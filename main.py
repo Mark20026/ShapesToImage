880fb10bb46882282a13c044dd06d1d69c656db5
Flatmates_Bill <bogimark2002@gmail.com>
2023-08-21 16:58:41 +0200

    Made the CLI, to be more interactive

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Rectangle:
         self.color = color
 
     def draw(self, canvas):
-        canvas.data[self.x: self.x + self.height, self.y: self.y + self.width] = self.color
+        canvas.data[self.y: self.y + self.height, self.x: self.x + self.width] = self.color
 
 
 class Square:
@@ -43,12 +43,46 @@ class Canvas:
         img.save(imagepath)
 
 
-canvas = Canvas(200, 300, color=(255, 255, 255))
+canvas_width = int(input("Canvas width: "))
+canvas_height = int(input("Canvas height: "))
+canvas_color = input("What color should be the canvas? (black or white)?: ")
 
-rectangle = Rectangle(10, 20, 20, 30, color=(255, 0, 0))
-rectangle.draw(canvas)
+if canvas_color == "white":
+    color = (255,255,255)
+elif canvas_color == "black":
+    color = (0,0,0)
 
-square = Square(10, 30, 50, (56, 25, 194))
-square.draw(canvas)
+canvas = Canvas(canvas_width, canvas_height, color=color)
 
-canvas.make("asd.png")
+while True:
+    user_draw = input("What do you want to draw (square or rectangle)? If you want to quit enter quit : ")
+
+    if user_draw == "square":
+        square_x = int(input("x coordinate: "))
+        square_y = int(input("y coordinate: "))
+        square_side = int(input("Side length: "))
+        square_red = int(input("How many red should be in it?: (0-255)"))
+        square_green = int(input("How many green should be in it?: (0-255)"))
+        square_blue = int(input("How many blue should be in it?: (0-255)"))
+
+        square = Square(square_x, square_y, square_side, color=(square_red, square_green, square_blue))
+        square.draw(canvas)
+
+    elif user_draw == "rectangle":
+        rectangle_x = int(input("x coordinate: "))
+        rectangle_y = int(input("y coordinate: "))
+        rectangle_width = int(input("Width: "))
+        rectangle_height = int(input("Height: "))
+        rectangle_red = int(input("How many red should be in it?: (0-255)"))
+        rectangle_green = int(input("How many green should be in it?: (0-255)"))
+        rectangle_blue = int(input("How many blue should be in it?: (0-255)"))
+
+        rectangle = Rectangle(rectangle_x, rectangle_y, rectangle_height, rectangle_width, color=(rectangle_red,
+                                                                                                  rectangle_green,
+                                                                                                  rectangle_blue))
+        rectangle.draw(canvas)
+
+    elif user_draw == "quit":
+        canvas.make("canvas.png")
+        break
+
