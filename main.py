5cbbb73f5f4a28c2a04b9f2caad28d5a7aac05fb
Flatmates_Bill <bogimark2002@gmail.com>
2023-08-21 17:10:25 +0200

    Gave comments

--- main.py
+++ main.py
@@ -3,8 +3,28 @@ from PIL import Image
 
 
 class Rectangle:
+    """
+    A class representing a rectangle shape.
+
+    Attributes:
+        x (int): The x-coordinate of the top-left corner of the rectangle.
+        y (int): The y-coordinate of the top-left corner of the rectangle.
+        height (int): The height of the rectangle.
+        width (int): The width of the rectangle.
+        color (tuple): The color of the rectangle in RGB format.
+    """
 
     def __init__(self, x, y, height, width, color):
+        """
+        Initialize a rectangle.
+
+        Args:
+            x (int): The x-coordinate of the top-left corner of the rectangle.
+            y (int): The y-coordinate of the top-left corner of the rectangle.
+            height (int): The height of the rectangle.
+            width (int): The width of the rectangle.
+            color (tuple): The color of the rectangle in RGB format.
+        """
         self.x = x
         self.y = y
         self.width = width
@@ -12,48 +32,113 @@ class Rectangle:
         self.color = color
 
     def draw(self, canvas):
-        canvas.data[self.y: self.y + self.height, self.x: self.x + self.width] = self.color
+        """
+        Draw the rectangle on the canvas.
+
+        Args:
+            canvas (Canvas): The canvas on which to draw the rectangle.
+        """
+        x_start = int(self.x)
+        y_start = int(self.y)
+        x_end = int(self.x + self.width)
+        y_end = int(self.y + self.height)
+        canvas.data[y_start: y_end, x_start: x_end] = self.color
 
 
 class Square:
+    """
+    A class representing a square shape.
+
+    Attributes:
+        x (int): The x-coordinate of the top-left corner of the square.
+        y (int): The y-coordinate of the top-left corner of the square.
+        side (int): The length of each side of the square.
+        color (tuple): The color of the square in RGB format.
+    """
 
     def __init__(self, x, y, side, color):
+        """
+        Initialize a square.
+
+        Args:
+            x (int): The x-coordinate of the top-left corner of the square.
+            y (int): The y-coordinate of the top-left corner of the square.
+            side (int): The length of each side of the square.
+            color (tuple): The color of the square in RGB format.
+        """
         self.x = x
         self.y = y
         self.side = side
         self.color = color
 
     def draw(self, canvas):
-        canvas.data[self.x: self.x + self.side, self.y: self.y + self.side] = self.color
+        """
+        Draw the square on the canvas.
+
+        Args:
+            canvas (Canvas): The canvas on which to draw the square.
+        """
+        x_start = int(self.x)
+        y_start = int(self.y)
+        x_end = int(self.x + self.side)
+        y_end = int(self.y + self.side)
+        canvas.data[y_start: y_end, x_start: x_end] = self.color
+
 
 
 class Canvas:
+    """
+    A class representing a canvas for drawing shapes.
+
+    Attributes:
+        width (int): The width of the canvas.
+        height (int): The height of the canvas.
+        color (tuple): The background color of the canvas in RGB format.
+        data (numpy.ndarray): The pixel data of the canvas.
+    """
 
     def __init__(self, width, height, color):
+        """
+        Initialize a canvas.
+
+        Args:
+            width (int): The width of the canvas.
+            height (int): The height of the canvas.
+            color (tuple): The background color of the canvas in RGB format.
+        """
         self.width = width
         self.height = height
         self.color = color
 
-        self.data = np.zeros((self.width, self.height, 3), dtype=np.uint8)
-
+        self.data = np.zeros((self.height, self.width, 3), dtype=np.uint8)
         self.data[:] = self.color
 
     def make(self, imagepath):
+        """
+        Save the canvas as an image file.
+
+        Args:
+            imagepath (str): The path where the image will be saved.
+        """
         img = Image.fromarray(self.data, "RGB")
         img.save(imagepath)
 
 
+# Get canvas dimensions and color from user input
 canvas_width = int(input("Canvas width: "))
 canvas_height = int(input("Canvas height: "))
 canvas_color = input("What color should be the canvas? (black or white)?: ")
 
+# Determine canvas color based on user input
 if canvas_color == "white":
     color = (255,255,255)
 elif canvas_color == "black":
     color = (0,0,0)
 
+# Create a canvas object with the specified dimensions and color
 canvas = Canvas(canvas_width, canvas_height, color=color)
 
+# Loop to allow drawing shapes on the canvas
 while True:
     user_draw = input("What do you want to draw (square or rectangle)? If you want to quit enter quit : ")
 
@@ -83,6 +168,7 @@ while True:
         rectangle.draw(canvas)
 
     elif user_draw == "quit":
+        # Save the canvas as an image file and exit the loop
         canvas.make("canvas.png")
         break
 
